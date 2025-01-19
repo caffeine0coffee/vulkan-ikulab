2e2306b3a35c1ea5f4b0fba223296dd97cc05a24
caffeine0coffee <dev@caffeinezero.info>
2025-01-19 16:53:50 +0900

    fix(build): develop preset name specification

--- scripts/prepare_assets_for_develop.sh
+++ scripts/prepare_assets_for_develop.sh
@@ -4,12 +4,12 @@ script_dir=$(dirname "${0}")
 project_dir=$(realpath "${script_dir}/..")
 cd "${project_dir}" || exit
 
-resource_dir_for_dev="$(cmake --preset=build_develop -N | grep RESOURCE_DIR | sed -n 's/.*"\(.*\)".*/\1/p')"
+resource_dir_for_dev="$(cmake --preset=develop -N | grep RESOURCE_DIR | sed -n 's/.*"\(.*\)".*/\1/p')"
 mkdir -p "${resource_dir_for_dev}"
 
 # copy fonts
 cp -r "${project_dir}/assets/fonts" "${resource_dir_for_dev}/fonts"
 
 # generate version info file
-cmake --preset=build_develop
+cmake --preset=develop
 cmake --build build_develop --target write_version_info_for_develop
