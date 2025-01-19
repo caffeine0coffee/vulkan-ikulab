82415f88470e3de643b58271dd88c98a5ef22b37
caffeine0coffee <dev@caffeinezero.info>
2025-01-19 16:42:21 +0900

    refactor(build): enclose variables w/ {}

--- scripts/prepare_assets_for_develop.sh
+++ scripts/prepare_assets_for_develop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-script_dir=$(dirname "$0")
-project_dir=$(realpath "$script_dir/..")
-cd "$project_dir" || exit
+script_dir=$(dirname "${0}")
+project_dir=$(realpath "${script_dir}/..")
+cd "${project_dir}" || exit
 
 resource_dir_for_dev="$(cmake --preset=build_develop -N | grep RESOURCE_DIR | sed -n 's/.*"\(.*\)".*/\1/p')"
-mkdir -p "$resource_dir_for_dev"
+mkdir -p "${resource_dir_for_dev}"
 
 # copy fonts
-cp -r "$project_dir/assets/fonts" "$resource_dir_for_dev/fonts"
+cp -r "${project_dir}/assets/fonts" "${resource_dir_for_dev}/fonts"
 
 # generate version info file
 cmake --preset=build_develop
