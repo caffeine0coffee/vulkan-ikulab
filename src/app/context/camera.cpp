0d058d01694c616c56f5dac89fe0dfbda8ffc910
caffeine0coffee <dev@caffeinezero.info>
2025-02-26 00:05:51 +0900

    refactor: magic number to const variable

--- src/app/context/camera.cpp
+++ src/app/context/camera.cpp
@@ -57,12 +57,14 @@ void Camera::UpdateCamera(const std::shared_ptr<Mouse>& mouse_context,
                 x_diff *= kRotationDiffRatio;
                 y_diff *= kRotationDiffRatio;
 
+                constexpr double kVRotationMargin = 0.0001;
+
                 h_rotation_ =
                     std::fmod(h_rotation_ - x_diff, 2.0 * std::numbers::pi);
                 v_rotation_ = std::clamp(
                     std::fmod(v_rotation_ + y_diff, 2.0 * std::numbers::pi),
-                    -std::numbers::pi / 2.0 + 0.0001,
-                    std::numbers::pi / 2.0 - 0.0001);
+                    -std::numbers::pi / 2.0 + kVRotationMargin,
+                    std::numbers::pi / 2.0 - kVRotationMargin);
             }
         }
         distance_ *= std::pow(kScrollRatio, -mouse_context->scrollOffsetY);
